9cbacc88b31d2fb22c178b10d747e9e5141029ef
Adam Linkous <alinkous@gmail.com>
2016-08-20 09:42:04 -0500

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# delugeserver
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'delugeserver'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures delugeserver'
+long_description 'Installs/Configures delugeserver'
+version '0.1.0'
