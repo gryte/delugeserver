ef51e727c2da1d5fd5683f6a6278d4208013b927
Adam Linkous <alinkous@gmail.com>
2016-09-30 20:03:17 -0500

    update_readme - update the readme
    also add inline documentation to the attributes file
    bump version
    initial release 1.0.0

--- README.md
+++ README.md
@@ -1,4 +1,39 @@
-# delugeserver
+# delugeserver Cookbook
 
-TODO: Enter the cookbook description here.
+This cookbook installs and configures a Deluge server.
 
+## Installation
+
+CentOS 7:
+
+```bash
+run the default recipe
+```
+
+## Usage example
+
+Use the attributes to help flesh out some of the install options with a Deluge server.
+
+### Attributes
+In order to keep the README manageable and in sync with the attributes, this cookbook documents attributes inline. The usage instructions and default values for attributes can be found in the individual attribute files.
+
+## Development setup
+
+Use Kitchen for integration testing.
+
+```bash
+kitchen test
+```
+
+## Release History
+
+* 1.0.0
+    * Initial Release
+
+## Meta
+
+Adam Linkous – [@gryte_meowmix](https://twitter.com/gryte_meowmix) – alinkous+support@gmail.com
+
+Distributed under the MIT license. See ``LICENSE`` for more information.
+
+[https://github.com/gryte/delugeserver](https://github.com/gryte/delugeserver)
--- attributes/default.rb
+++ attributes/default.rb
@@ -1,15 +1,42 @@
-# manage auth file users
+#
+#    manage auth file users
+#
+# If set to true, then the cookbook will create the file
+# /var/lib/deluge/.config/deluge/auth based on the auth.erb which requires an
+# encrypted data bag 'auth'. Please see test/integration/data_bags/auth for an
+# example.
+#
 default['deluge']['config']['auth'] = false
 
-# manage configuration files
+#
+#    manage configuration files
+#
+# This will create and update the files /var/lib/deluge/.config/deluge/core.conf
+# and /var/lib/deluge/.config/deluge/label.conf. The core.conf file is updated
+# with items from 'settings' via the deluge-console. The label.conf is hard-coded
+# with a label.conf.erb file and requires the current values specified in 'directories'.
+# See .kitchen.yml for examples.
+#
 default['deluge']['config']['core.conf']['manage'] = false
 default['deluge']['config']['core.conf']['settings'] = {}
 default['deluge']['config']['label.conf'] = false
 
-# manage plugins
+#
+#    manage plugins
+#
+# List of plugins to enable via the deluge-console. The label.conf file is directly
+# related to this attribute (meaning there's no point configuring the label plugin
+# if you don't actually enable it).
+# See .kitchen.yml for examples.
+#
 default['deluge']['plugin']['enable'] = []
 
-# manage directory creation
+#
+#    manage directory creation
+#
+# List of directories to create along with the owner and group. This is directly
+# related to the core.conf and label.conf files.
+#
 default['deluge']['app']['directories'] = {
   '/.deluge' => {
     'owner' => 'deluge',
