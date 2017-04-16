f5e218d1572e876fe05b1cd29265f3c2f2266321
Adam Linkous <alinkous@gmail.com>
2017-04-15 22:37:40 -0500

    specify chef version 12 pessimistic
    Signed-off-by: Adam Linkous <alinkous@gmail.com>

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email 'alinkous+support@gmail.com'
 license 'all_rights'
 description 'Installs/Configures delugeserver'
 long_description 'Installs/Configures delugeserver'
-version '1.1.3'
+version '1.1.4'
 supports 'centos'
-chef_version '~> 13.0' if respond_to?(:chef_version)
+chef_version '~> 12.19' if respond_to?(:chef_version)
 issues_url 'https://github.com/gryte/delugeserver/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/gryte/delugeserver' if respond_to?(:source_url)
