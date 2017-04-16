1872dd943cc092a4649cd5adf457cdef71d75bcd
Adam Linkous <alinkous@gmail.com>
2017-04-15 20:39:04 -0500

    update test server ip; bump version
    Signed-off-by: Adam Linkous <alinkous@gmail.com>

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'alinkous+support@gmail.com'
 license 'all_rights'
 description 'Installs/Configures delugeserver'
 long_description 'Installs/Configures delugeserver'
-version '1.1.0'
+version '1.1.1'
 issues_url 'https://github.com/gryte/delugeserver/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/gryte/delugeserver' if respond_to?(:source_url)
