e88f34d24c09ddf9f8b7a659588391f520227eb1
Adam Linkous <alinkous@gmail.com>
2017-04-15 21:09:31 -0500

    update test server ip; bump version (#11)
    * update test server ip; bump version
    
    Signed-off-by: Adam Linkous <alinkous@gmail.com>
    
    * satisfy various cookstyle and foodcritic findings
    
    Signed-off-by: Adam Linkous <alinkous@gmail.com>

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,8 @@ maintainer_email 'alinkous+support@gmail.com'
 license 'all_rights'
 description 'Installs/Configures delugeserver'
 long_description 'Installs/Configures delugeserver'
-version '1.1.0'
+version '1.1.2'
+supports 'centos'
+chef_version '>= 13.0' if respond_to?(:chef_version)
 issues_url 'https://github.com/gryte/delugeserver/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/gryte/delugeserver' if respond_to?(:source_url)
