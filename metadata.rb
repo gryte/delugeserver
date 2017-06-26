3d98cdd8714c1d03f0dc8dde7d7c209fd8041e3f
Adam Linkous <alinkous@gmail.com>
2017-06-25 22:48:42 -0500

    upgrade deluge-console package resource; bump version
    Signed-off-by: Adam Linkous <alinkous@gmail.com>

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'alinkous+support@gmail.com'
 license 'all_rights'
 description 'Installs/Configures delugeserver'
 long_description 'Installs/Configures delugeserver'
-version '2.0.0'
+version '2.0.1'
 supports 'centos'
 chef_version '~> 12.19' if respond_to?(:chef_version)
 issues_url 'https://github.com/gryte/delugeserver/issues' if respond_to?(:issues_url)
