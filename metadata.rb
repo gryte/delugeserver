edbe8620cf4466087351561eb324ac46c7cc78dd
Adam Linkous <alinkous@gmail.com>
2018-04-20 19:49:51 -0500

    update metadata.rb chef version constraint
    - change to optimistically support 12.19 and up
    - bump patch version
      - bug fix

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'alinkous+support@gmail.com'
 license 'MIT'
 description 'Installs/Configures delugeserver'
 long_description 'Installs/Configures delugeserver'
-version '2.1.1'
+version '2.1.2'
 supports 'centos'
-chef_version '~> 14.0' if respond_to?(:chef_version)
+chef_version '>= 12.19' if respond_to?(:chef_version)
 issues_url 'https://github.com/gryte/delugeserver/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/gryte/delugeserver' if respond_to?(:source_url)
 
